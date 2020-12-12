22da5f7dc6a28418c7376c481dc88aadc339042a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-12 05:02:47 -0500

    Update final.js

--- js/final.js
+++ js/final.js
@@ -2,7 +2,7 @@ let rectangle = {
     //Initialize paddle object variables
     x: 0,
     y: 550,
-    w: 150,
+    w: 80,
     h: 25,
 
     update: function(){
@@ -50,27 +50,13 @@ function Ball(){
     this.collideY = function(){
         if(this.y<this.d){
             this.velY *= -1;
+            this.y = this.d;
         }
     }
     this.collidePaddle = function(rectX,rectY,rectW,rectH){
         if(this.y + this.d > rectY && this.y - this.d < rectY + rectH && this.x + this.d > rectX && this.x - this.d < rectX + rectW) {
-            //if(this.x<rectX + (rectW/4)){
-            //    this.velX = -6;
-            //    this.velY = -2;
-            //}else if(this.x>rectX + (rectW/4)&& this.x<rectX + (rectW/2)){
-            //    this.velX = -4;
-            //    this.velY = -4;
-            //}else if(this.x>rectX + (rectW/2)&& this.x<rectX + (rectW * .75)){
-            //    this.velX = 4;
-            //    this.velY = -4;
-            //}else{
-            //    this.velX = 6;
-            //    this.velY = -2;
-            //}
-
-            this.velX = (((this.x-(rectX+(rectW/2)))/rectW)*15)
-            //this.velY = (10 - Math.abs(this.velX)) * -1
-            this.velY = -10
+            this.velX = (((this.x-(rectX+(rectW/2)))/rectW)*15);
+            this.velY = -10;
             
         } 
     }
@@ -82,7 +68,11 @@ function Ball(){
             if(blocks[i].health > 0){
                 if (this.x + this.d > blocks[i].x && this.x - this.d < blocks[i].x + blocks[i].w && this.y + this.d > blocks[i].y && this.y - this.d < blocks[i].y + blocks[i].h){
                     if(blocks[i].canBeHit){
-                        blocks[i].health -= 1;
+                        if(blocks[i].breakable){
+                            blocks[i].health -= 1;
+                            levelCreator.blockCount --;
+                            score ++;
+                        }
                         this.currentBlocksCollide ++;
                         if (blocks[i].ballDir == 1){
                             if(this.currentBlocksCollide==1){
@@ -98,8 +88,6 @@ function Ball(){
                                 this.velY *= -1;
                             }
                         }
-                        levelCreator.blockCount --;
-                        score ++;
                         blocks[i].canBeHit = false;
                     }
                 }else{
@@ -113,7 +101,7 @@ function Ball(){
         this.x = 400;
         this.y = 400;
         this.velX = 0;
-        this.velY = 2;
+        this.velY = 0;
     }
     this.lose = function(){
         if (this.y-this.d>600){
@@ -148,66 +136,41 @@ function Block(x,y,health){
     this.color = "red";
     this.ballDir = 0;
     this.canBeHit = true;
+    this.breakable = true;
+    this.standard = true;
 
     this.update = function(){
-        switch(this.health){
-            case 1:
-                this.color = "#FFC300";
-                break;
-            case 2:
-                this.color = "#FF5733";
-                break;
-            case 3:
-                this.color = "#C70039";
-                break;
-            case 4:
-                this.color = "#900C3E";
-                break;
-            case 5:
-                this.color = "#571845";
-                break;
-            default:
-                this.color = "red";
+        if(this.breakable && this.standard){
+            switch(this.health){
+                case 1:
+                    this.color = "#FFC300";
+                    break;
+                case 2:
+                    this.color = "#FF5733";
+                    break;
+                case 3:
+                    this.color = "#C70039";
+                    break;
+                case 4:
+                    this.color = "#900C3E";
+                    break;
+                case 5:
+                    this.color = "#571845";
+                    break;
+                case 6:
+                    this.color = "#2E0C68";
+                    break;
+                default:
+                    this.color = "red";
+            }
+        }else{
+            this.color = "gray";
         }
         if(this.health > 0){
             fill(this.color);
             rect(this.x,this.y,this.w,this.h);
-            //console.log(this.health);
         }
     }
-    /*this.collideBall = function(ballX, ballY, ballD){
-        if (this.health > 0){
-            if (ballX + ballD > this.x && ballX - ballD < this.x + this.w && ballY + ballD > this.y && ballY - ballD < this.y + this.h){
-                if(this.canBeHit){
-                    this.health -= 1;
-                    if (this.ballDir == 1){
-                        ball.velY *= -1;
-                        levelCreator.blockCount --;
-                        score ++;
-                        timer = 0;
-                        this.canBeHit = false;
-                    }else if(this.ballDir == 2){
-                        ball.velX *= -1;
-                        levelCreator.blockCount --;
-                        score ++;
-                        timer = 0;
-                        this.canBeHit = false;
-                    }else{
-                        ball.velX *= -1;
-                        ball.velY *= -1;
-                        levelCreator.blockCount --;
-                        score++;
-                        timer = 0;
-                        this.canBeHit = false;
-                    }
-                }
-            }else{
-                ball.blockCollide = false;
-                this.canBeHit = true;
-            }
-
-        }
-    }*/
     this.senseBall = function(ballX, ballY, ballD, ballCollide){
         if(!ballCollide){
             if(ballX + ballD > this.x && ballX - ballD < this.x + this.w){
@@ -222,11 +185,13 @@ function Block(x,y,health){
 }
 
 let levelCreator = {
-    currentLevel: 0,
+    currentLevel: 3,
     levelActive: true,
     blockCount: 0,
 
-    levels: [[0,0,0,0,0,0,0,0,
+    levels: [
+            [0,0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,0,
              0,0,0,0,0,0,0,0,
              0,0,0,0,0,0,0,0,
              0,0,0,0,0,0,0,0],
@@ -234,19 +199,35 @@ let levelCreator = {
             [0,0,0,0,0,0,0,0,
              1,1,1,1,1,1,1,1,
              1,1,1,1,1,1,1,1,
+             1,1,1,1,1,1,1,1,
              0,0,0,0,0,0,0,0],
             
             [1,1,1,1,1,1,1,1,
              1,1,1,1,1,1,1,1,
+             2,2,2,2,2,2,2,2,
              1,1,1,1,1,1,1,1,
              1,1,1,1,1,1,1,1],
+            
+            [3,3,3,3,3,3,3,3,
+             2,2,2,2,2,2,2,2,
+             1,9,9,1,1,9,9,1,
+             2,1,1,2,2,1,1,2,
+             1,1,1,1,1,1,1,1],
+            
+            [0,0,0,0,0,0,0,0,
+             0,9,9,9,9,9,0,0,
+             0,9,1,0,0,0,0,0,
+             0,9,9,9,9,9,9,9,
+             0,0,0,0,0,0,0,0],
              
-            [5,4,5,4,5,4,5,4,
+            [6,5,6,5,6,5,6,5,
+             5,4,5,4,5,4,5,4,
              4,3,4,3,4,3,4,3,
              3,2,3,2,3,2,3,2,
              2,1,2,1,2,1,2,1],
             
             [5,5,5,5,5,5,5,5,
+             5,5,5,5,5,5,5,5,
              5,5,5,5,5,5,5,5,
              5,5,5,5,5,5,5,5,
              5,5,5,5,5,5,5,5]
@@ -278,13 +259,23 @@ let levelCreator = {
                     blocks[n].health = 5;
                     this.blockCount += 5;
                     break;
+                case 6:
+                    blocks[n].health = 6;
+                    this.blockCount += 6;
+                    break;
+                case 9:
+                    blocks[n].health = 6;
+                    blocks[n].breakable = false;
+                    break;
                 default:
                     blocks[n].health = 0;
+            }
+            if(this.levels[this.currentLevel][n] != 9){
+                blocks[n].breakable = true;
             }
         }
     },
     levelClearCheck: function(){
-        //console.log(this.blockCount)
         if(this.blockCount == 0){
             this.levelAdvance();
         }
@@ -297,10 +288,11 @@ let levelCreator = {
 }
 
 let ball = new Ball();
-let blocks = [block0 = new Block(0,100,0), block1 = new Block(100,100,0), block2 = new Block(200,100,0), block3 = new Block(300,100,0), block4 = new Block(400,100,0), block5 = new Block(500,100,0), block6 = new Block(600,100,0), block7 = new Block(700,100,0),
-              block8 = new Block(0,150,0), block9 = new Block(100,150,0), block10 = new Block(200,150,0), block11 = new Block(300,150,0), block12 = new Block(400,150,0), block13 = new Block(500,150,0), block14 = new Block(600,150,0), block15 = new Block(700,150,0),
-              block16 = new Block(0,200,0), block17 = new Block(100,200,0), block18 = new Block(200,200,0), block19 = new Block(300,200,0), block20 = new Block(400,200,0), block21 = new Block(500,200,0), block22 = new Block(600,200,0), block23 = new Block(700,200,0),
-              block24 = new Block(0,250,0), block25 = new Block(100,250,0), block26 = new Block(200,250,0), block27 = new Block(300,250,0), block28 = new Block(400,250,0), block29 = new Block(500,250,0), block30 = new Block(600,250,0), block31 = new Block(700,250,0)]
+let blocks = [block0 = new Block(0,50,0), block1 = new Block(100,50,0), block2 = new Block(200,50,0), block3 = new Block(300,50,0), block4 = new Block(400,50,0), block5 = new Block(500,50,0), block6 = new Block(600,50,0), block7 = new Block(700,50,0),
+              block8 = new Block(0,100,0), block9 = new Block(100,100,0), block10 = new Block(200,100,0), block11 = new Block(300,100,0), block12 = new Block(400,100,0), block13 = new Block(500,100,0), block14 = new Block(600,100,0), block15 = new Block(700,100,0),
+              block16 = new Block(0,150,0), block17 = new Block(100,150,0), block18 = new Block(200,150,0), block19 = new Block(300,150,0), block20 = new Block(400,150,0), block21 = new Block(500,150,0), block22 = new Block(600,150,0), block23 = new Block(700,150,0),
+              block24 = new Block(0,200,0), block25 = new Block(100,200,0), block26 = new Block(200,200,0), block27 = new Block(300,200,0), block28 = new Block(400,200,0), block29 = new Block(500,200,0), block30 = new Block(600,200,0), block31 = new Block(700,200,0),
+              block32 = new Block(0,250,0), block33 = new Block(100,250,0), block34 = new Block(200,250,0), block35 = new Block(300,250,0), block36 = new Block(400,250,0), block37 = new Block(500,250,0), block38 = new Block(600,250,0), block39 = new Block(700,250,0)]
 
 let score = 0;
 let lives = 2;
@@ -308,18 +300,14 @@ let timer = 0;
 
 function setup(){
     createCanvas(800,600);
-    //levelCreator.generate();
 }
 
 function draw(){
     background("black");
     strokeWeight(0);
     rectangle.update();
-    //block.update();
-    //block.collideBall(ball.x,ball.y,ball.d);
     for(let i = 0; i<blocks.length; i++){
         blocks[i].update();
-        //blocks[i].collideBall(ball.x,ball.y,ball.d);
         blocks[i].senseBall(ball.x,ball.y,ball.d,ball.blockCollide);
     }
     ball.update();
